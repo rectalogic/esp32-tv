8595705171d5b95dfa0c4babf58a409af9a2faf7
Andrew Wason <rectalogic@rectalogic.com>
2026-06-09 13:09:28 -0400

    use board_build.embed_files
    fails with: Error: The program size (4020457 bytes) is greater than
    maximum allowed (1310720 bytes)

--- player/src/EmbeddedVideo.cpp
+++ player/src/EmbeddedVideo.cpp
@@ -2,23 +2,11 @@
 
 #ifdef USE_EMBED
 
-#define STRINGIFY_VALUE(value) #value
-#define STRINGIFY(value) STRINGIFY_VALUE(value)
-#define EMBEDDED_VIDEO_RESOURCE STRINGIFY(EMBEDDED_VIDEO)
+extern const uint8_t video_avi_start[] asm("_binary_src_video_avi_start");
+extern const uint8_t video_avi_end[] asm("_binary_src_video_avi_end");
 
-#if !defined(__has_embed)
-#error "USE_EMBED requires compiler support for the C++ #embed directive"
-#else
-#if !__has_embed(EMBEDDED_VIDEO_RESOURCE)
-#error "EMBEDDED_VIDEO resource was not found"
-#else
-const uint8_t EMBEDDED_VIDEO_DATA[] = {
-#embed EMBEDDED_VIDEO_RESOURCE
-};
+const uint8_t *EMBEDDED_VIDEO_DATA = video_avi_start;
 
-const size_t EMBEDDED_VIDEO_LENGTH = sizeof(EMBEDDED_VIDEO_DATA);
-const char *EMBEDDED_VIDEO_NAME = STRINGIFY(EMBEDDED_VIDEO);
-#endif
-#endif
-
+const size_t EMBEDDED_VIDEO_LENGTH = video_avi_end - video_avi_start;
+const char *EMBEDDED_VIDEO_NAME = "video.avi";
 #endif
--- player/src/EmbeddedVideo.h
+++ player/src/EmbeddedVideo.h
@@ -4,11 +4,7 @@
 
 #ifdef USE_EMBED
 
-#ifndef EMBEDDED_VIDEO
-#error "USE_EMBED requires EMBEDDED_VIDEO to name the AVI resource"
-#endif
-
-extern const uint8_t EMBEDDED_VIDEO_DATA[];
+extern const uint8_t *EMBEDDED_VIDEO_DATA;
 extern const size_t EMBEDDED_VIDEO_LENGTH;
 extern const char *EMBEDDED_VIDEO_NAME;
 
